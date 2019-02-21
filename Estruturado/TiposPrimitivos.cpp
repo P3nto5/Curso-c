471c37135dd0bc52d86831af55b2656bee6a121f
samuel <sam226gold@gmail.com>
2019-02-21 09:46:29 -0400

    Tipos primitivos

---
+++ Estruturado/TiposPrimitivos.cpp
@@ -0,0 +1,20 @@
+//tipos de dados primitivos
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	//tipo inteiro
+	int NumeroInteiro = 1;
+	//tipo real
+	float NumeroReal = 2.0;
+	//tipo caracter
+	char TipoCaracter = 'a';
+	//Tipo bool
+	bool Verifica = true;
+	
+	printf("Inteiro %d \n", NumeroInteiro );//retorna inteiro
+	printf("Real %f \n", NumeroReal );//retorna real
+	printf("Caracter %c \n", TipoCaracter );// retorna caracter
+	printf("Boolean %x \n", Verifica );// retorna 1 ou 0 ou seja true e  1 e false e 0
+	system("pause");	
+}
+
